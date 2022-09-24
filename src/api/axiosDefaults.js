d34338810f77528e8ea570034dfe3e1c1e0dcef5
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2022-09-24 11:59:52 +0000

    change settings

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://recipe-star-3.herokuapp.com/";
+axios.defaults.baseURL = "https://recipe-star-3-api.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
